962f43925b43a99fd7dcba69b268a279fd731561
Summerdise <350464517@qq.com>
2020-09-14 23:05:08 +0800

    [zmc] feat: add return null when get empty string function

---
+++ src/main/java/Mommifier.java
@@ -0,0 +1,14 @@
+public class Mommifier {
+    String input;
+
+    public Mommifier(String input) {
+        this.input = input;
+    }
+
+    public String getResult() {
+        if (input == null || input.equals("")) {
+            return null;
+        }
+        return null;
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MommifierTest {
+
+    @Test
+    public void should_return_null_when_get_empty_string() {
+        Mommifier mommifier = new Mommifier("");
+        String getString = mommifier.getResult();
+        assertEquals(null, getString);
+    }
 }
